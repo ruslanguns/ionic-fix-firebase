a45a06a62120d1dd15667fdbc2fea3e7e2762ce5
Antonio Gonzalez <toni@wificlick.es>
2021-11-16 20:39:18 +0000

    Configuración Ionic PWA

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { defineCustomElements } from '@ionic/pwa-elements/loader'; // importamos el PWA libreria
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
@@ -8,5 +9,9 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.log(err));
+
+// Cargas el loader de PWA con el window del browser
+defineCustomElements(window);
